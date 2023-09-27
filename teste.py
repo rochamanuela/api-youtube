f72588ab4004f0658868f097b9fbb78e7a05b919
rochamanuela <rochadesouzamanuela@gmail.com>
2023-09-27 16:57:10 -0300

    27/09/2023 - 16:57

--- teste.py
+++ teste.py
@@ -1,17 +1,17 @@
 import requests
 
-# Substitua 'SUA_CHAVE_DE_API' pela sua chave de API do YouTube
 API_KEY = 'AIzaSyB2bwXAFvbficNF1riIZD9NP_ugcvgmez4'
 
 # URL da API para busca de vídeos
 url = 'https://www.googleapis.com/youtube/v3/search'
 
 pesquisa = input('Digite o termo de busca: ')
+print('\n\n')
 
 # Parâmetros da solicitação
 params = {
     'key': API_KEY,
-    'q': pesquisa,  # Substitua 'termo_de_busca' pela sua consulta de busca
+    'q': pesquisa,
     'type': 'video',        # Você pode especificar o tipo de resultado (vídeo, canal, playlist, etc.)
     'part': 'snippet',      # Partes dos dados que você deseja incluir na resposta (snippet é comum)
     'maxResults': 10        # Número máximo de resultados a serem retornados
@@ -27,7 +27,7 @@ data = response.json()
 for item in data['items']:
     video_id = item['id']['videoId']
     video_title = item['snippet']['title']
-    print(f'Video ID: {video_id}')
+    # print(f'Video ID: {video_id}')
     print(f'Título do Vídeo: {video_title}')
     print(f'Link do Vídeo: https://www.youtube.com/watch?v={video_id}')
     print('---')
